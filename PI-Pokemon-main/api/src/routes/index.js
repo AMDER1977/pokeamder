bd782a99d72b1f53ebf41a84c419a30a951e62db
Amderson Hernandez <ahernandezdep@gmail.com>
2023-10-13 16:06:39 -0500

    "a"

--- PI-Pokemon-main/api/src/routes/index.js
+++ PI-Pokemon-main/api/src/routes/index.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
 const pokemonsRoutes = require("./pokemonsRoutes")
 const typesRouters = require("./typesRoutes") 
 const router = Router();
+
+
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
